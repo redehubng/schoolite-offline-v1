2c880cb00b99e2328708a88cf8b8079d3f913bd2
alabamustapha <alabamustapha@gmail.com>
2016-11-05 09:13:32 +0100

    view clear command

--- storage/framework/views/43025a5b496f9c1e95757737491a0d668151d57a.php
+++
@@ -1,5 +0,0 @@
-<div class="footer text-center">
-    <div>
-        <strong>Copyright</strong> <a href="http://www.redehubng.com" target="_blank" title="Rede Hub">Redehubng</a> &copy; 2016
-    </div>
-</div>
